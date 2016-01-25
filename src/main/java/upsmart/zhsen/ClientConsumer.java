b0749b10c049286161e19edf8a79238d2c7ae805
zhangsen <zhangsen@unionpaysmart.com>
2016-01-25 15:14:05 +0800

    first commit

---
+++ src/main/java/upsmart/zhsen/ClientConsumer.java
@@ -0,0 +1,45 @@
+package upsmart.zhsen;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.ConsumerCancelledException;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.ShutdownSignalException;
+
+public class ClientConsumer {
+
+	private final static String QUEUE_NAME = "hello";
+	
+	/**
+	 * @param args
+	 * @throws TimeoutException 
+	 * @throws IOException 
+	 * @throws InterruptedException 
+	 * @throws ConsumerCancelledException 
+	 * @throws ShutdownSignalException 
+	 */
+	public static void main(String[] args) throws IOException, TimeoutException, ShutdownSignalException, ConsumerCancelledException, InterruptedException {
+		// TODO Auto-generated method stub
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+		QueueingConsumer consumer = new QueueingConsumer(channel);
+		channel.basicConsume(QUEUE_NAME, true, consumer);
+		
+		while (true) {
+			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+			String message = new String(delivery.getBody());
+			System.out.println(" [x] Received '" + message + "'");
+		}
+
+	}
+
+}
